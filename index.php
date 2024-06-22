12763fdc5915d74026f736bdcdc9161b551f0f50
Carl <carljustinem984@gmail.com>
2024-06-23 00:54:31 +0800

    UNderstood how to connect css

--- index.php
+++ index.php
@@ -6,17 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home | Appendix Colonial Hotel</title>
 
-        <!-- CSS for index-->
-        <link rel="stylesheet" href="/assets/CSS/index.css">
-
-        <!-- CSS for header-->
-        <link rel="stylesheet" href="/assets/CSS/header.css">
-
-        <!-- CSS for footer-->
-        <link rel="stylesheet" href="/assets/CSS/footer.css">
-
-        <!-- General CSS-->
-        <link rel="stylesheet" href="/assets/CSS/style.css">
 
         <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -24,6 +13,8 @@
 </head>
 
 <body>
+        <!-- Header -->
+        <?php include './assets/resources/header.php'; ?>
 
 
 
