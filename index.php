15e1267b520b78c468c36b46689b3220ec3c2756
Carl <carljustinem984@gmail.com>
2024-06-22 23:58:41 +0800

    Added some files

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home | Appendix Colonial Hotel</title>
+
+        <!-- CSS for index-->
+        <link rel="stylesheet" href="/assets/CSS/index.css">
+
+        <!-- CSS for header-->
+        <link rel="stylesheet" href="/assets/CSS/header.css">
+
+        <!-- CSS for footer-->
+        <link rel="stylesheet" href="/assets/CSS/footer.css">
+
+        <!-- General CSS-->
+        <link rel="stylesheet" href="/assets/CSS/style.css">
+
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+
+
+
+</body>
+
+<!-- Bootstrap JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<!-- JS for Index -->
+<script src="./assets/JS/index.js"></script>
+
+<!-- JS for Headers -->
+<script src="./assets/JS/header.js"></script>
+
+</html>
