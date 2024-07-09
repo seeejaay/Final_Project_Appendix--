53a0c1a8c84b5bcb56cc7e35b26fd459e0373ce9
Carl <carljustinem984@gmail.com>
2024-07-10 00:24:34 +0800

    update

--- client/booking.php
+++ client/booking.php
@@ -127,7 +127,23 @@
                         </div>
 
 
-                        <!-- Confirmation Modal -->
+                        <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="successModalLabel">Booking Success</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Your booking has been successfully completed!</p>
+                                        <p>Transaction ID: <span id="transactionID"></span></p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-primary" id="successOkButton">OK</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
