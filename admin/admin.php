7eece13817f15902a967e74fa732ebabc9fef6df
Dale Tambis <clayd0423@gmail.com>
2024-07-11 01:31:11 +0800

    changed

--- admin/admin.php
+++ admin/admin.php
@@ -20,14 +20,56 @@ include '../assets/resources/dbConfig.php';
     <link rel="stylesheet" href="../assets/CSS/header.css">
     <link rel="stylesheet" href="../assets/CSS/viewBooking.css">
     <link rel="stylesheet" href="../assets/CSS/footer.css">
-
+    <!-- AdminLTE CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/css/adminlte.min.css">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     <!-- Navbar FOR ALL-->
-    <?php include '../assets/resources/adminheader.php'; ?>
+    <aside class="main-sidebar sidebar-dark-primary elevation-9" style="background-color: #1c1610;">
+        <!-- Brand Logo -->
+        <a href="admin.php" class="brand-link" style="text-align: center">
+            <img src="../assets/images/logo_bg.png" style="width: 128px; height: auto; padding-bottom: 12px;"> <br>
+            <span class="brand-text font-weight-light">Timeless Elegance</span>
+        </a>
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <!-- Sidebar Menu -->
+            <nav class="mt-4">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false"">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-solid fa-pen-to-square"></i>
+                            <p>Edit Transaction</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.php?page=view" class="nav-link">
+                            <i class="nav-icon fas fa-solid fa-receipt"></i>
+                            <p>View All Transaction</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-solid fa-ban"></i>
+                            <p>Cancel Booking</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-sign-out-alt"></i>
+                            <p>Logout</p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </aside>
 
     <main class="container-wrapper">
         <div class="top">
