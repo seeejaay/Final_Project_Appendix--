e2b34db709ea2bfb3dd2a189ce7546d0443043be
Carl <carljustinem984@gmail.com>
2024-07-11 11:26:36 +0800

    update

--- admin/header3.php
+++ admin/header3.php
@@ -10,11 +10,17 @@
         <!-- Sidebar Menu -->
         <nav class="mt-4 custom-width">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false"">
-                    <li class=" nav-item">
-                <a href="admin.php?page=viewAll_Trans" class="nav-link">
-                    <i class="nav-icon fas fa-solid fa-receipt"></i>
-                    <p>View All Transaction</p>
+                <li class=" nav-item">
+                <a href="../index.php   " class="nav-link">
+                    <i class="nav-icon fas fa-solid fa-home"></i>
+                    <p>Home</p>
                 </a>
+                </li>
+                <li class=" nav-item">
+                    <a href="admin.php?page=viewAll_Trans" class="nav-link">
+                        <i class="nav-icon fas fa-solid fa-receipt"></i>
+                        <p>View All Transaction</p>
+                    </a>
                 </li>
 
                 <li class="nav-item">
